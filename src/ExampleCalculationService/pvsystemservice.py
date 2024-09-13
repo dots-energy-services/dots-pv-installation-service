0f393cf2fc6160a079d8db2dad35658058dce106
Gijs Verhoeven <g.verhoeven.1@tue.nl>
2024-09-13 13:35:59 +0200

    add helperfunction vecotr param with name

--- src/ExampleCalculationService/pvsystemservice.py
+++ src/ExampleCalculationService/pvsystemservice.py
@@ -6,6 +6,7 @@ import logging
 from dots_infrastructure.DataClasses import EsdlId, HelicsCalculationInformation, PublicationDescription, SubscriptionDescription, TimeStepInformation
 from dots_infrastructure.HelicsFederateHelpers import HelicsSimulationExecutor
 from dots_infrastructure.Logger import LOGGER
+from dots_infrastructure.CalculationServiceHelperFunctions import get_vector_param_with_name
 from esdl import EnergySystem
 
 class CalculationServicePVSystem(HelicsSimulationExecutor):
@@ -77,8 +78,9 @@ class CalculationServicePVSystem(HelicsSimulationExecutor):
     def predict_solar_power(self, param_dict : dict, simulation_time : datetime, time_step_number : TimeStepInformation, esdl_id : EsdlId, energy_system : EnergySystem):
         LOGGER.info("calculation 'predict_solar_power' started")
         LOGGER.info(param_dict)
+        LOGGER.info(get_vector_param_with_name(param_dict, "solar_irradiance"))
         # Receive solar irradiance data from param_dict.
-        solar_irradiance = param_dict["solar_irradiance"]
+        solar_irradiance = get_vector_param_with_name(param_dict, "solar_irradiance")
         surface_area = self.surface_area[esdl_id]
         panel_efficiency = self.panel_efficiency[esdl_id]
         # # temporary for test without esdl:
